dbcdd13e3488dab1ea50367525d0154c7c65cc89
majjy <majjy@DESKTOP-QDK17VI>
2025-12-08 22:18:16 -0500

    created multiply test

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -10,6 +10,7 @@ public class SimpleCalculatorTest {
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
-		assertEquals(calc.minus(5, 3), 2); 
+		assertEquals(calc.minus(5, 3), 2);
+		assertEquals(calc.multiply(1, 1), 1); 
 	}
 }
