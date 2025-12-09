54fd057dca20b980d373587359e4e154edec1216
majjy <majjy@DESKTOP-QDK17VI>
2025-12-08 22:22:27 -0500

    failiing divide test

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -12,5 +12,6 @@ public class SimpleCalculatorTest {
 		assertEquals(calc.add(1, 1), 2);
 		assertEquals(calc.minus(5, 3), 2);
 		assertEquals(calc.multiply(1, 1), 1); 
+		assertEquals(calc.divide(1, 1), 1); 
 	}
 }
